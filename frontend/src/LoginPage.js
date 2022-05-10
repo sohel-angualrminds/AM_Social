314cb3b504d898b3ec1ca1da8b3835c6c47d21a1
anuja@angularminds.in <anuja@angularminds.in>
2022-05-10 18:23:12 +0530

    update

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -12,7 +12,7 @@ import MuiAlert from '@mui/material/Alert'
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
-
+ 
 function LoginPage(props) {
     const navigate = useNavigate()
     const { checkLoggedIn } = props;
